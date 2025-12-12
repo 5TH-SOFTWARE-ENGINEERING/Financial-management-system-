f5ad851ebcd7aa70dcae567904f2c582c012028a
amarebirhane <amarebirhane89@gmail.com>
2025-12-12 11:09:56 +0300

    updated the user-roles of user-roles

---
+++ frontend/app/settings/users-roles/user-roles/page.tsx
@@ -0,0 +1,9 @@
+import ClientPage from './ClientPage';
+
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+export default function UserRolesPage() {
+  return <ClientPage />;
+}
+
